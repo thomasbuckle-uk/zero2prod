64226b7d2543b1a90978d3bfa4ae5a5ff0345652
Thomas Buckle <thomasbuckle.uk@gmail.com>
2023-10-14 11:09:26 +0100

    Refactoring + added health_check test!

---
+++ tests/health_check.rs
@@ -0,0 +1,25 @@
+use zero2prod::run;
+
+fn spawn_app() {
+    let server = run().expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+}
+#[tokio::test]
+async fn health_check_works() {
+   spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to exectute request. ");
+
+
+    // Assert
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
