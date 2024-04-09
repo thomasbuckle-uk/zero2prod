b93a9a80f4e903ba038476f218ea006313699388
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-04-09 09:59:21 +0100

    Chapter 3!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
@@ -5,9 +6,18 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+        .listen(listener)?
         .run();
+
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,24 +1,93 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
-
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    // Arrange
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
-        .expect("Failed to exectute request. ");
+        .expect("Failed to execute request.");
 
     // Assert
-
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_date() {
+    // Tests really are very similar to PHPUnit & Pest testing frameworks
+
+
+    // Setup
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Run
+    let body = "name=thomas%20buckle&email=thomasbuckle.uk%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assertions
+    assert_eq!(200, response.status().as_u16());
+}
+
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+
+    // An example of table-driven test also known as a parametrised test.
+    // We've done tests like these before in Symfony/Laravel land testing the shape of responses
+
+    // Setup
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=thomas%20buckle", "missing the email"),
+        ("email=thomasbuckle.uk%40gmail.com", "missing the name"),
+        ("", "missing both a name and email"),
+    ];
+
+
+    for (invalid_body, error_message) in test_cases {
+
+        // Run
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assertions
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API didn't fail with 400 Bad Request when payload was {:?}.",
+            error_message
+        )
+    }
 }
