0023e941a15a8eb5ce0aa36ea79b5abc5cd7e4dd
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-04-11 09:41:16 +0100

    Added method to return postgres connection string applied fmt formatting

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    
     let configuration = get_configuration().expect("Failed to read configuration.");
-    
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener)?.await
