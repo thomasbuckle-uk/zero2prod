82dd3d848ddb5c3323f3b2142f237083a6b7a0ca
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-04-11 09:53:52 +0100

    Updated tests updated github workflows added .env

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 fn spawn_app() -> String {
@@ -31,11 +33,18 @@ async fn health_check_works() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_date() {
+async fn subscribe_returns_a_200_for_valid_form_data() {
     // Tests really are very similar to PHPUnit & Pest testing frameworks
 
     // Setup
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let client = reqwest::Client::new();
 
     // Run
@@ -50,6 +59,14 @@ async fn subscribe_returns_a_200_for_valid_form_date() {
 
     // Assertions
     assert_eq!(200, response.status().as_u16());
+    
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    
+    assert_eq!(saved.email, "thomasbuckle.uk@gmail.com");
+    assert_eq!(saved.name, "thomas buckle");
 }
 
 #[tokio::test]
