98903acb30aba13f5ee05aa30ea92812673ca73a
Thomas Buckle <thomasbuckle.uk@outlook.com>
2024-04-18 10:28:05 +0100

    Added tests for domain.rs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -155,7 +155,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 #[tokio::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // Setup
-    
+
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
@@ -163,7 +163,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
         ("name=Thomas&email=", "empty email"),
         ("name=Thomas&email=definitely-not-an-email", "invalid email"),
     ];
-    
+
     for (body, description) in test_cases {
         let response = client
             .post(&format!("{}/subscriptions", &app.address))
@@ -172,7 +172,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             .send()
             .await
             .expect("Failed to execute request.");
-        
+
         assert_eq!(
             400,
             response.status().as_u16(),
